077ae1ae3b0cfe2a0691d4c25eca1b7b0a785c32
olem90 <olemariusrognan90@gmail.com>
2023-04-26 00:54:14 +0100

    added all files to my folders

---
+++ src/js/api/constants.mjs
@@ -0,0 +1,5 @@
+export const API_HOST_URL = "https://api.noroff.dev";
+export const API_BASE = "/api/v1";
+export const API_AUCTION_BASE = "/auction";
+export const API_SOCIAL_URL = `${API_HOST_URL}${API_BASE}${API_AUCTION_BASE}`;
+export const listingsURL = "/listings";
